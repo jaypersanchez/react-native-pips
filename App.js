84bd70238d18a98cf52df13dcd91cdb22d9e312e
jaypersanchez <jaypersanchez@gmail.com>
2020-08-04 21:28:50 -0600

    create wallet listing created wallet but not able to get web3 to work and now ios simulator is messing up but can test on browser

--- App.js
+++ App.js
@@ -1,19 +1,64 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import React, { useState } from 'react';
+import { Alert, Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import Web3 from 'web3'
 
 export default function App() {
+
+  var web3 = new Web3('http://localhost:8545')
+
+  const styles = StyleSheet.create({
+    title: {
+      marginTop: 16,
+      paddingVertical: 8,
+      color: "#20232a",
+      textAlign: "center",
+      fontSize: 30,
+      fontWeight: "bold",
+      alignItems: 'center'
+    }
+
+  });
+
+  const [newWallet, listWallets] = useState([])
+  var [walletAddress, createWallet] = useState('')
+
+
+  function addWallet(address) {
+    //now add new wallet address to list of wallets
+    listWallets( newWallet => [...newWallet, address] )
+    Alert.alert(`Wallet ${address} added to list`)
+  }
+
+  /*
+  * Will need to import web3 and ethers
+  */
+  function generateWalletAddress()  {
+    var address = '0X0';
+    //address = web3.eth.accounts.create()
+    createWallet(address);
+    //now add new wallet to list of wallets
+    addWallet(address);
+    //Alert.alert('create new wallet :: ' + {walletAddress});
+  }
+
   return (
     <View style={{flexDirection: 'column', padding:50}}>
 
-      <View style={{borderColor:'black',borderWidth:1, padding:10}}>
-        <TextInput placeholder='wallet address' />
+      <View style={styles.title}>
+        <Text>Your Wallet Addresses</Text>
       </View>
-      <Button title='Purchase' />
+      <Button title='Create New Wallet' 
+              onPress={generateWalletAddress} />
+
+      <View>
+        {newWallet.map( (wallet) => <Text key={wallet}>{wallet}</Text> )}
+      </View>
 
     </View>
-    
+  
   );
+
 }
 
 
